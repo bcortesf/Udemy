0301aff9e27ea4fb18b6cc0fd7775e99a2bf2e32
carvajal/Bryan.Cortes <bryan.cortes@carvajal.com>
2023-04-18 17:28:24 -0500

    hibernate consulta entity<persona>
    creacion de entidad<persona>, se consulta con find y se crean querys de consulta

--- BACK/workspace-sts-4-4.18.0-bryan/JPA/Domingo-Sebastian/code/01-hello-world/src/main/java/com/develop_bcf/jpa/entityDominio/Persona.java
+++ BACK/workspace-sts-4-4.18.0-bryan/JPA/Domingo-Sebastian/code/01-hello-world/src/main/java/com/develop_bcf/jpa/entityDominio/Persona.java
@@ -7,15 +7,33 @@ import jakarta.persistence.Id;
 public class Persona {
 	
 	@Id
+	public int id;
 	public String nombre;
+	public String pais;
+
 
 	// Alt + Shift + s
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
 	public String getNombre() {
 		return nombre;
 	}
-
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
+	public String getPais() {
+		return pais;
+	}
+	public void setPais(String pais) {
+		this.pais = pais;
+	}
 	
+	@Override
+	public String toString() {
+		return "Persona [id=" + id + ", nombre=" + nombre + ", pais=" + pais + "]";
+	}
 }
--- BACK/workspace-sts-4-4.18.0-bryan/JPA/Domingo-Sebastian/code/01-hello-world/src/main/java/com/develop_bcf/jpa/main/Main.java
+++ BACK/workspace-sts-4-4.18.0-bryan/JPA/Domingo-Sebastian/code/01-hello-world/src/main/java/com/develop_bcf/jpa/main/Main.java
@@ -1,32 +1,89 @@
 package com.develop_bcf.jpa.main;
 
+import java.util.List;
+
 import com.develop_bcf.jpa.entityDominio.Persona;
 
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
+import jakarta.persistence.Query;
 
 public class Main {
 
 	public static void main(String[] args) {
-		//guardarPersona();
+//		guardarPersona();
+//		consultarPersona1();
+		consultarPersona2();
 		
 
+	}
+
+	public static void consultarPersona2() {
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("jpa_main");
 		EntityManager em = emf.createEntityManager();
 		em.getTransaction().begin();
-			Persona persona = new Persona();
-			persona.setNombre("Bryan");
+			Persona persona1 = new Persona();
+			Persona persona2 = new Persona();
+			Persona persona3 = new Persona();
+			persona1.setId(1);	persona1.setNombre("Bryan");	persona1.setPais("COL");
+			persona2.setId(2);  persona2.setNombre("Moticas");	persona2.setPais("ANGELES");
+			persona3.setId(3);  persona3.setNombre("Minnie");	persona3.setPais("COL");
 			
-			em.persist(persona);
+			em.persist(persona1);
+			em.persist(persona2);
+			em.persist(persona3);
 			em.getTransaction().commit();
+
+
+			//https://www.logicbig.com/tutorials/java-ee-tutorial/jpa/select-from-where-statement.html
+			Query queryAll = em.createQuery("SELECT e FROM Persona e");
+			Query queryByName = em.createQuery("SELECT e FROM Persona e WHERE e.nombre = :peopleName");
+
+
+			//Buscar todas las personas
+			@SuppressWarnings("unchecked")
+			List<Persona> resultList = queryAll.getResultList();
+			resultList.forEach(System.out::println);
+			//Buscar persona por nombre
+			queryByName.setParameter("peopleName", "Minnie");
+
+			/**
+			 * https://stackoverflow.com/questions/1607532/when-to-use-entitymanager-find-vs-entitymanager-getreference-with-jpa
+			 * find			: realiza interno un SELECT, porque se necesita por ejemplo para un DTO salida
+			 * getReference : crea referencia sin hacer un SELECT, si solo se necesita el <primaryKey>, para UPDATE,DELETE;
+			 * 				  crea referencia y hace un SELECT, si se usa un getter distinto al Entity.<primaryKey>;
+			 */
+			Persona entity1 = em.find(Persona.class, 1); //<Entity>,<primaryKey= Persona.id>
+			Persona entity2 = em.getReference(Persona.class, 2);
+			System.out.println();
+			System.out.println(entity1.getNombre());
+			System.out.println(entity2.getNombre() + "\n");
 			
-			Persona findPersona = em.find(Persona.class, "Bryan");
-			System.out.println(findPersona.getNombre());
+			Persona entity3 = (Persona) queryByName.getSingleResult();
+			System.out.println(entity3.getNombre());
+			System.out.println("tipo de clase entity3:\t" + entity3.getClass().getName());
 		em.close();
-		emf.close();
+		emf.close();		
 	}
 	
+	public static void consultarPersona1() {
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("jpa_main");
+		EntityManager em = emf.createEntityManager();
+		em.getTransaction().begin();
+			Persona persona1 = new Persona();
+			persona1.setNombre("Bryan");
+			
+			em.persist(persona1);
+			em.getTransaction().commit();
+			
+			Persona entity1 = em.find(Persona.class, "Bryan");
+			System.out.println(entity1.getNombre());
+		em.close();
+		emf.close();		
+	}
+	
+	// https://stackoverflow.com/questions/72692978/eclipse-project-project-name-has-no-explicit-encoding-set
 	public static void guardarPersona() {
 		/**
 		 * /01-hello-world/src/main/resources/META-INF/persistence.xml
