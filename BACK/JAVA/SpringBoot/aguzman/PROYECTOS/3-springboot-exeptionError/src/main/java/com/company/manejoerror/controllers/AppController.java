7a270229df6b9bf3946379c1fb1638d5419a0599
Bryan.Cortés <bcortesf.developer@gmail.com>
2024-02-04 17:02:55 -0500

    (manejo-Exepcion): Exepciones en el controlador + Exepcion personalizada lanzada por un throw RuntimeExeption

--- BACK/JAVA/SpringBoot/aguzman/PROYECTOS/3-springboot-exeptionError/src/main/java/com/company/manejoerror/controllers/AppController.java
+++ BACK/JAVA/SpringBoot/aguzman/PROYECTOS/3-springboot-exeptionError/src/main/java/com/company/manejoerror/controllers/AppController.java
@@ -29,15 +29,33 @@ public class AppController {
     }
 
 
+    
     @GetMapping(path = "/200-5")
-    public Object metodo5() {
+    public Object metodo5() throws ErrorCustomRuntimeException {
         LocalDate createDate = LocalDate.now().plusDays(1);//1: vencido, 0:esVigente
         LocalDate expirateDate = LocalDate.now();
         boolean isCurrent = (createDate.isBefore(expirateDate) || createDate.isEqual(expirateDate));
         if (!isCurrent) {
             String mensaje = MessageFormat.format("La fechaCreacion {0} es mayor a la fechaVencimiento {1}."
                 , createDate, expirateDate);
-            error.setError("FECHA EXPIRADA");
+            errorRUNEX.setError("error 505: ".concat("FECHA EXPIRADA"));
+            errorRUNEX.setMessage(mensaje);
+            errorRUNEX.setStatus(HttpStatus.INTERNAL_SERVER_ERROR.value() + 5);
+            errorRUNEX.setDate(expirateDate);
+            throw errorRUNEX;
+        }
+        return "fecha de vencimiento de la carne, vigente";
+    }
+    @GetMapping(path = "/200-4")
+    public Object metodo4() {
+        //NO-HANDLER-EXCEPTION-CONTROLLER
+        LocalDate createDate = LocalDate.now().plusDays(1);//1: vencido, 0:esVigente
+        LocalDate expirateDate = LocalDate.now();
+        boolean isCurrent = (createDate.isBefore(expirateDate) || createDate.isEqual(expirateDate));
+        if (!isCurrent) {
+            String mensaje = MessageFormat.format("La fechaCreacion {0} es mayor a la fechaVencimiento {1}."
+                , createDate, expirateDate);
+            error.setError("error 504: ".concat(" FECHA EXPIRADA"));
             error.setMessage(mensaje);
             error.setStatus(HttpStatus.INTERNAL_SERVER_ERROR.value());
             error.setDate(expirateDate);
@@ -45,34 +63,25 @@ public class AppController {
         }
         return "fecha de vencimiento de la carne, vigente";
     }
-    @GetMapping(path = "/200-4")
-    public Object metodo4() throws ErrorCustomRuntimeException {
-        LocalDate createDate = LocalDate.now().plusDays(1);//1: vencido, 0:esVigente
-        LocalDate expirateDate = LocalDate.now();
-        boolean isCurrent = (createDate.isBefore(expirateDate) || createDate.isEqual(expirateDate));
-        if (!isCurrent) {
-            String mensaje = MessageFormat.format("La fechaCreacion {0} es mayor a la fechaVencimiento {1}."
-                , createDate, expirateDate);
-            errorRUNEX.setError("FECHA EXPIRADA");
-            errorRUNEX.setMessage(mensaje);
-            errorRUNEX.setStatus(HttpStatus.INTERNAL_SERVER_ERROR.value());
-            errorRUNEX.setDate(expirateDate);
-            throw errorRUNEX;
-        }
-        return "fecha de vencimiento de la carne, vigente";
+
+
+    @GetMapping(path = "/200-3b")
+    public Object metodo3b() {
+        Integer divisor = null;
+        return 2024 / divisor;
     }
-
-
-    @GetMapping(path = "/200-3")
-    public Object metodo3() {
+    @GetMapping(path = "/200-3a")
+    public Object metodo3a() {
         return 2024 / 0;
     }
     @GetMapping(path = "/200-2")
     public Object metodo2() {
         return Integer.valueOf("uno");
     }
+
     @GetMapping(path = "/200-1")
     public Object metodo1() {
+        //NO-HANDLER-EXCEPTION-CONTROLLER
         try {
             Integer.valueOf("uno");
         } catch (Exception e) {
--- BACK/JAVA/SpringBoot/aguzman/PROYECTOS/3-springboot-exeptionError/src/main/java/com/company/manejoerror/controllers/HandlerExceptionController.java
+++ BACK/JAVA/SpringBoot/aguzman/PROYECTOS/3-springboot-exeptionError/src/main/java/com/company/manejoerror/controllers/HandlerExceptionController.java
@@ -1,35 +1,75 @@
 package com.company.manejoerror.controllers;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import com.company.manejoerror.models.ErrorCustom;
 import com.company.manejoerror.models.ErrorCustomRuntimeException;
 
 @RestControllerAdvice
 public class HandlerExceptionController {
+    private ErrorCustom errorCustomDTO;
 
-    
+    public HandlerExceptionController(ErrorCustom errorCustomDTO) {
+        this.errorCustomDTO = errorCustomDTO;
+    }
+
+
     @ExceptionHandler(value = ErrorCustomRuntimeException.class) //atrapa exepcion del controlador
-    public ResponseEntity<ErrorCustomRuntimeException> expirateDateError(ErrorCustomRuntimeException err) {
-        return ResponseEntity.internalServerError().body(err);
-    }
-
-
-    @ExceptionHandler
-    public ResponseEntity<String> convertTypeError(NumberFormatException ex) {
-        return ResponseEntity.internalServerError()
-            .body("error 500: ".concat(ex.getMessage()));
-    }
-    @ExceptionHandler
-    public ResponseEntity<String> unknowError(Exception ex) {
-        return ResponseEntity.internalServerError()
-            .body("error 1989: EXEPCION-NO-CONTROLADA:\n".concat(ex.toString()));
+    public ResponseEntity<ErrorCustom> expirateDateError(ErrorCustomRuntimeException err) { //505
+        errorCustomDTO.setError(err.getError());
+        errorCustomDTO.setMessage(err.getMessage());
+        errorCustomDTO.setStatus(err.getStatus());
+        errorCustomDTO.setDate(err.getDate());
+        // return ResponseEntity.internalServerError().body(errorCustomDTO); //FORMA-1
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorCustomDTO); //FORMA-2
     }
     /////////////////////////////////////////////////////////////
-    @ExceptionHandler(value = ArithmeticException.class)
-    public ResponseEntity<?> divideByZero() {
-        return new ResponseEntity<>("mi objeto", HttpStatus.INTERNAL_SERVER_ERROR);
+    @ExceptionHandler(value = NullPointerException.class)
+    public ResponseEntity<ErrorCustom> dataNull(NullPointerException err) {
+        errorCustomDTO.setError("error 503b: ".concat(err.getMessage()));
+        errorCustomDTO.setMessage(err.fillInStackTrace().toString());
+        errorCustomDTO.setStatus(HttpStatus.INTERNAL_SERVER_ERROR.value());
+        errorCustomDTO.setDate(LocalDate.now());
+        return ResponseEntity.internalServerError().body(errorCustomDTO);
     }
+    @ExceptionHandler(value = ArithmeticException.class)
+    public ResponseEntity<?> divideByZero(ArithmeticException err) {
+        return new ResponseEntity<>("error 503a:".concat(err.getMessage())
+            , HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+    /////////////////////////////////////////////////////////////
+    // @ExceptionHandler
+    // public ResponseEntity<String> convertTypeError(NumberFormatException ex) {
+    //     return ResponseEntity.internalServerError()
+    //         .body("error 502: ".concat(ex.getMessage()));
+    // }
+
+    @ExceptionHandler(NumberFormatException.class)
+    @ResponseStatus(code = HttpStatus.INTERNAL_SERVER_ERROR) //postman
+    public Map<String, Object> convertTypeError(NumberFormatException ex) {
+        Map<String, Object> error = new HashMap<>();
+        error.put("dateTime", LocalDateTime.now());
+        error.put("error", "error 502: numero invalido o incorrecto; no tiene formato de digito!");
+        error.put("message", ex.getMessage());
+        error.put("status", HttpStatus.INTERNAL_SERVER_ERROR.value()); //mi-respuesta
+        return error;
+    }
+
+
+    @ExceptionHandler
+    public ResponseEntity<String> unknowError(Exception ex) {
+        //Cualquier tipo de exepcion que no se tenga atrapada y/o controlada
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+            .body("error 404-89: EXEPCION-NO-CONTROLADA:\n".concat(ex.toString()));
+    }
+    /////////////////////////////////////////////////////////////
 }
