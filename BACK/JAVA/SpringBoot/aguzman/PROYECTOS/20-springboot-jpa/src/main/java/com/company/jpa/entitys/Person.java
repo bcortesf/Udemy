3c49341476029724024210cc304d8f88eb70b961
Bryan.Cortés <bcortesf.developer@gmail.com>
2024-02-18 15:52:04 -0500

    Revert "En clase Entity<Persona> crear campos de trazabilidad o auditoria"
    This reverts commit e65227c61e563ea8aeec05f0249f82d1155f8f6e.
    
    (JPA-1): Entity<Persona> crear campos <Trazabilidad-Auditoria>,
    con metodos para pre-persistir BD (@prePersist,@preUpdate,@preRemove)

--- BACK/JAVA/SpringBoot/aguzman/PROYECTOS/20-springboot-jpa/src/main/java/com/company/jpa/entitys/Person.java
+++ BACK/JAVA/SpringBoot/aguzman/PROYECTOS/20-springboot-jpa/src/main/java/com/company/jpa/entitys/Person.java
@@ -1,16 +1,12 @@
 package com.company.jpa.entitys;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.PrePersist;
-import jakarta.persistence.PreRemove;
-import jakarta.persistence.PreUpdate;
 import jakarta.persistence.Table;
 
 
@@ -26,17 +22,9 @@ public class Person {
     private Long id;
     private String name;
     private String lastname;
-    @Column(name = "programming_language") //MYSQL:(bd->campo->nombre)
+    @Column(name = "programming_language") //bd.campo.nombre
     private String programmingLanguage;
     private LocalDate birthdate;
-
-    /*MANEJAR TABLA<Person> PARA:   TRAZAR(Trazabililidad) ó AUDITAR(Auditoria) */
-    @Column(name = "created_at")
-    private LocalDateTime createdAt;
-    @Column(name = "updated_at")
-    private LocalDateTime updatedAt;
-    @Column(name = "deleted_at")
-    private LocalDateTime deletedAt;
 
 
     public Person() {
@@ -58,24 +46,6 @@ public class Person {
         this.name = name;
         this.programmingLanguage = programmingLanguage;
     }
-
-
-    @PrePersist
-    public void prePersist() {
-        System.out.println("evento del CICLO-DE-VIDA del entity: pre-persist");
-        this.createdAt = LocalDateTime.now();
-    }
-    @PreUpdate
-    public void preUpdate() {
-        System.out.println("evento del CICLO-DE-VIDA del entity: pre-update");
-        this.updatedAt =  LocalDateTime.now();
-    }
-    @PreRemove
-    public void preRemove() {
-        System.out.println("evento del CICLO-DE-VIDA del entity: pre-remove");
-        this.deletedAt = LocalDateTime.now();
-    }
-
 
     public Long getId() {
         return id;
