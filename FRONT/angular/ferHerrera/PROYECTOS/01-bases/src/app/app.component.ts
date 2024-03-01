e705353efdfb7564ce7ea93f0ba4a2418d4e90a3
Bryan.Cortés <bcortesf.developer@gmail.com>
2024-03-01 18:08:54 -0500

    (angular): separar logica de app-root  y  llamar componente de  app-counter

--- FRONT/angular/ferHerrera/PROYECTOS/01-bases/src/app/app.component.ts
+++ FRONT/angular/ferHerrera/PROYECTOS/01-bases/src/app/app.component.ts
@@ -7,43 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   public title :string = 'Hola mundo';
-  public counter :number = 10;
   public persona :{} =  {
     id: 1,
     nombre: 'John',
     apellido: 'McKinley'
   };
 
-  increaseBy(value: number) :void {
-    if (this.counter === 0) return;
-    this.counter += value;
-  }
-
-  resetCounter(){
-    this.counter = 10;
-  }
-
-  //---------------------------------------
-  //---------------------------------------
-  calculate(type: string, value: number) :void {
-    //->1.Establece logica
-    const operations :Operations = {
-      increment: () => {
-        this.counter += value;
-      },
-      decrement: () => {
-        if (this.counter === 0) return;
-        this.counter += value;
-      }
-    };
-
-    //->2.Ejecuta logica
-    const operation = operations[type];
-    (operation)
-      ? operation()
-      : console.error(`Type of invalid operation: ${type}`);
-  }
-}
-interface Operations {
-    [type: string]: () => void;
 }
--- FRONT/angular/ferHerrera/PROYECTOS/01-bases/src/app/counter/counter.component.ts
+++ FRONT/angular/ferHerrera/PROYECTOS/01-bases/src/app/counter/counter.component.ts
@@ -1,10 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-counter',
   templateUrl: './counter.component.html',
   styleUrl: './counter.component.css'
 })
-export class CounterComponent {
 
+export class CounterComponent implements OnInit {
+  public counter :number = 10;
+
+  constructor() { }
+  ngOnInit() { }
+
+
+  increaseBy(value: number) :void {
+    if (this.counter === 0) return;
+    this.counter += value;
+  }
+
+  resetCounter(){
+    this.counter = 10;
+  }
+
+  //---------------------------------------
+  //---------------------------------------
+  calculate(type: string, value: number) :void {
+    //->1.Establece logica
+    const operations :Operations = {
+      increment: () => {
+        this.counter += value;
+      },
+      decrement: () => {
+        if (this.counter === 0) return;
+        this.counter += value;
+      }
+    };
+
+    //->2.Ejecuta logica
+    const operation = operations[type];
+    (operation)
+      ? operation()
+      : console.error(`Type of invalid operation: ${type}`);
+  }
+}
+interface Operations {
+    [type: string]: () => void;
 }
