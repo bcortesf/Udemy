ef5691d7d72767a29c38f1c3ca8583281c8c9a85
Bryan.Cortés <bcortesf.developer@gmail.com>
2024-03-04 00:14:03 -0500

    (angular): refactorizacion de servicios e implementacion en un page-component con hijos con @input,@output

--- FRONT/angular/ferHerrera/PROYECTOS/01-bases/src/app/dbz/pages/main-page.component.ts
+++ FRONT/angular/ferHerrera/PROYECTOS/01-bases/src/app/dbz/pages/main-page.component.ts
@@ -10,9 +10,22 @@ import { DbzService } from '../services/dbz.service';
   styleUrl: './main-page.component.css'
 })
 export class MainPageComponent {
+  constructor(private dbzService: DbzService) {
+  }
 
-  constructor(public dbzService: DbzService) {
+  get characters(): Character[] {
+    return [...this.dbzService.characters]; //opcional: para no modificar la data del service
+  }
 
+
+  onNewCharacter(character :Character) {
+    this.dbzService.onNewCharacter(character);
+  }
+  onDeleteCharacterByIndex(indexArray :number) :void {
+    this.dbzService.onDeleteCharacterByIndex(indexArray);
+  }
+  onDeleteCharacterByUUID(UUIDcharacter :string) :void {
+    this.dbzService.onDeleteCharacterByUUID(UUIDcharacter);
   }
 }
 
@@ -39,10 +52,10 @@ export class MainPageComponent_OLD {
     // this.characters.push(character)
   }
 
-  onDeleteCharacterByIndex(indexArray :number) {
+  onDeleteCharacterByIndex(indexArray :number) :void {
     this.characters.splice(indexArray, 1);
   }
-  onDeleteCharacterByUUID(UUIDcharacter :string) {
+  onDeleteCharacterByUUID(UUIDcharacter :string) :void {
     this.characters = this.characters.filter(
       // (character:Character) => ! (character.id!.includes(UUIDcharacter))
       (character:Character) => character.id !== UUIDcharacter
