98a78c3efd4e28bff0b08559f00f934c7cb8c5c7
Bryan.Cortés <bcortesf.developer@gmail.com>
2024-03-25 15:34:35 -0500

    (angular-form-multiple-selector): <router-oulet> = mostrar rutas de un modulo importado

--- FRONT/angular/ferHerrera/PROYECTOS/08-selectores/src/app/app.module.ts
+++ FRONT/angular/ferHerrera/PROYECTOS/08-selectores/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CountriesRoutingModule } from './countries/countries-routing.module';
 
 @NgModule({
   declarations: [
@@ -11,6 +12,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     AppRoutingModule
+    //-> 2.Importa el modulo que contiene "rutas" de navegacion
+    ,CountriesRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- FRONT/angular/ferHerrera/PROYECTOS/08-selectores/src/app/countries/countries-routing.module.ts
+++ FRONT/angular/ferHerrera/PROYECTOS/08-selectores/src/app/countries/countries-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SelectorPageComponent } from './pages/selector-page/selector-page.component';
 
-const routes: Routes = [];
+//-> 1.Define las rutas
+const routes: Routes = [
+  { path: 'hi' , component: SelectorPageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
